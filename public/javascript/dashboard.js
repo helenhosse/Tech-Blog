24f13afdcaefb22bac3b029c4f00a75cc0358cdf
Helen Hosse <helenhosse@gmail.com>
2023-04-22 21:32:48 -0600

    push for tutor

--- public/javascript/dashboard.js
+++ public/javascript/dashboard.js
@@ -15,7 +15,7 @@ const newFormHandler = async (event) => {
 		});
 
 		if (response.ok) {
-			document.location.replace('/dashboard');
+			document.location.replace('/views/dashboard');
 		} else {
 			alert('Failed post');
 		}
@@ -31,7 +31,7 @@ const delButtonHandler = async (event) => {
 		});
 
 		if (response.ok) {
-			document.location.replace('/dashboard');
+			document.location.replace('/views/dashboard');
 		} else {
 			alert('Failed deletion');
 		}
